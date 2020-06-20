537ba275bf90e329ebffbb4d6d8a260751b7f212
ceezuns <49824660+ceezuns@users.noreply.github.com>
2020-06-19 19:47:21 -0700

    Update to Spigot version 1.15.2, and update git credentials.

--- configure_git.sh
+++ configure_git.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 echo "1) Personal"
-echo "	ceezuns@gmail.com, 305E279D82819B20"
-
+echo "	ceezuns@gmail.com"
 echo ""
 
 echo "2) GitHub Provided"
-echo "	49824660+ceezuns@users.noreply.github.com, 15C9630ACADE5855"
-
+echo "	49824660+ceezuns@users.noreply.github.com"
 echo ""
 
 echo "Choose A Profile: "
-
 read profile
 
 if [ $profile == "1" ]; then
 	git config user.email "ceezuns@gmail.com"
-	git config user.signingkey "305E279D82819B20"
+	git config user.signingkey "2E0F398AFF319A59"
 elif [ $profile == "2" ]; then
 	git config user.email "49824660+ceezuns@users.noreply.github.com"
-	git config user.signingkey "15C9630ACADE5855"
+	git config user.signingkey "D38E310072F1AC23"
 else
-	echo "An Error Occurred, Please Relaunch The Script"
+	echo "The input was invalid, please relaunch the script!"
 fi
